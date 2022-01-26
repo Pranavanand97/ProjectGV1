3c16d16131ff15712a678f5768b0e5271008416a
haribotx <80144381+haribotx@users.noreply.github.com>
2022-01-26 20:31:00 +0530

    app

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 var jobsData =[];
 
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://script.google.com/macros/s/AKfycbx_aTT55MY4ej4nd59dikrjqE1iMCihhZuHpNBBwcBjuN3uLKg8j1cxuUQZCBxD4Bfv/exec');
+ourRequest.open('GET', 'https://script.google.com/u/0/home/projects/1iYAaGeuotilSY9TDN3VfE1QL14MxYF97jngvZfKi1IUAafmWZRPex972/edit');
 ourRequest.onload = function() {
   if (ourRequest.status >= 200 && ourRequest.status < 400) {
     const data = JSON.parse(ourRequest.responseText);
